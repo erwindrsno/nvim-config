3498df044dc04072bf94ee966ce21e9f4ef64434
Erwin Darsono <erwindarsono08@gmail.com>
2026-07-12 20:45:30 +0800

    change file name from kanagawa.lua to colorscheme.lua

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,20 @@
+-- return {
+--   { "rebelot/kanagawa.nvim" },
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "kanagawa",
+--     },
+--   },
+-- }
+--
+
+return {
+  { "folke/tokyonight.nvim" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight-night",
+    },
+  },
+}
